d5618e380c848dbeee0b93dab319e2633f7acdb0
Haitham Ayyad <hsa7@hotmail.com>
2023-09-04 13:56:57 +0300

    add field

---
+++ test/RecordEFW2C/Records/RCERecord/RCEFields/RceEstablishmentNumberCorrect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EFW2C.Common.Enum;
+using EFW2C.Extensions;
+using EFW2C.Records;
+
+namespace EFW2C.Fields
+{
+    //Created by : HSA 4-9-2023
+    //Reviewed by : 
+
+    public class RceEstablishmentNumberCorrect : FieldBase
+    {
+        public RceEstablishmentNumberCorrect(RecordBase record, string data)
+            : base(record, data)
+        {
+            _pos = 39;
+            _length = 4;
+        }
+
+        public override bool Verify()
+        {
+            if (!base.Verify())
+                return false;
+
+            return true;
+        }
+
+        protected override FieldTypeEnum GetFieldType()
+        {
+            return FieldTypeEnum.UpperCase_LeftJustify_Blank;
+        }
+
+        public override bool IsRequired()
+        {
+            return false;
+        }
+    }
+}
