223da80fa771dac4e54ccbc6103e05b0374df8ce
Haitham Ayyad <hsa7@hotmail.com>
2023-09-06 13:58:36 +0300

    Remove test

--- test/RecordEFW2C/BaseClasses/FieldBase.cs
+++ test/RecordEFW2C/BaseClasses/FieldBase.cs
@@ -12,7 +12,6 @@ namespace EFW2C.Fields
 {
     public abstract class FieldBase
     {
-        //test
         protected RecordBase _record;
         protected int _pos;
         protected int _length;
