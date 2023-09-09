1d42a9827677a52f39ac7121e398fcff45d5b97c
Haitham Ayyad <hsa7@hotmail.com>
2023-09-09 20:43:43 +0300

    add rest of Rcw fields

---
+++ test/RecordEFW2C/Records/RCWRecord/RCWFields/RcwStatutoryEmployeeIndicatorOriginal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EFW2C.Common.Enums;
+using EFW2C.Extensions;
+using EFW2C.Records;
+
+namespace EFW2C.Fields
+{
+    //Created by : 9-9-2023
+    //Reviewed by : 
+
+    public class RcwStatutoryEmployeeIndicatorOriginal : FieldOriginal
+    {
+        public RcwStatutoryEmployeeIndicatorOriginal(RecordBase record, string data)
+            : base(record, data)
+        {
+            _pos = 1002;
+            _length = 1;
+        }
+
+        public override bool Verify()
+        {
+            if (!base.Verify())
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ test/RecordEFW2C/Records/RCWRecord/RCWFields/RcwThirdPartySickPayndicatorCorrect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EFW2C.Common.Enums;
+using EFW2C.Extensions;
+using EFW2C.Records;
+
+namespace EFW2C.Fields
+{
+    //Created by : 9-9-2023
+    //Reviewed by : 
+
+    public class RcwThirdPartySickPayndicatorCorrect : FieldCorrect
+    {
+        public RcwThirdPartySickPayndicatorCorrect(RecordBase record, string data)
+            : base(record, data)
+        {
+            _pos = 1007;
+            _length = 1;
+        }
+
+        public override bool Verify()
+        {
+            if (!base.Verify())
+                return false;
+
+            var localData = DataInRecordBuffer();
+
+            if ((localData == "1" || localData == "0"))
+                throw new Exception($"{ClassName}: data only can be zero or one");
+
+            return true;
+        }
+    }
+}
