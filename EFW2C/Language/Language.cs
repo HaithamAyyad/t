74e59043e3cdf3fc624fa17038a31fda884b4ca1
Haitham Ayyad <hsa7@hotmail.com>
2023-10-07 15:08:58 +0300

    Add description

--- EFW2C/Language/Language.cs
+++ EFW2C/Language/Language.cs
@@ -11,9 +11,11 @@ namespace EFW2C.Resource.Language
     {
         private static Language _instance;
         private static ResourceManager _descpitionDesourceManager;
+        private static ResourceManager _exceptionsManager;
         private Language()
         {
             _descpitionDesourceManager = new ResourceManager("EFW2C.Resources.ResourceDescription", typeof(Language).Assembly);
+            _exceptionsManager = new ResourceManager("EFW2C.Resources.ResourceExceptions", typeof(Language).Assembly);
         }
 
         public static Language Instance
@@ -42,6 +44,26 @@ namespace EFW2C.Resource.Language
 
             }
             if(!str.Contains(old.Substring(3)))
+            {
+
+            }
+
+            return descriptionStr;
+        }
+        public string LoadExceptionString(string str)
+        {
+            var descriptionStr = "{Exception-Not-Defined}";
+            try
+            {
+                str = _descpitionDesourceManager.GetString(str);
+                if (str != null)
+                    descriptionStr = str;
+            }
+            catch
+            {
+            }
+
+            if(str == null)
             {
 
             }
